2b49e3c24cff68d4bfbf84c0b1400c5887f8b950
krista3200 <73085133+krista3200@users.noreply.github.com>
2021-03-07 11:32:48 -0600

    recreated react-weather-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Footer from "./Footer";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <div className="container"></div>
+      <form id="search-form">
+        <input type="text" placeholder="City Search" id="search"/>
+        <input type="Submit" id="submit-form" value="Search"/>
+        <button type="button" id="current-location"> Current Location </button>
+        <img src = "https://openweathermap.org/img/wn/10d@2x.png"
+                alt="icon">
+                </img>
+  <h1>
+  <div className="row"></div>
+  <div id="date" className="col-4"></div>
+  <div className="col-4"></div>
+  <div>Wind: <span id="wind"></span>km/h</div>
+  <div className="row"></div>
+  <div className="col-4"></div>
+  <p className="currently">
+  <div className="row"></div>
+  <div className="col-sm" id="c-f"></div>
+  <span className="temperature" id="temperature"></span>
+  <span className="units">
+    <a href="/" id = "celsius-link" className="active"> °C </a> | <a href="/" id = "fahrenheit-link">°F </a></span> 
+  <div className="col-sm" id="conditions"></div>
+  <div className="col-sm"></div>
+  <div>Humidity: <span id="humidity"></span>%</div><br />
+  </p>
+  <div className="row weather-forecast" id="forecast"></div>
+<div className="row row-cols-1 row-cols-md-5"></div>  
+  </h1> 
+<Footer />
+
+</form>
+  </div>         
+    
   );
 }
-
-export default App;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+<div>
+<small>
+  <a href="https://github.com/krista3200/weather-project" target="_blank">
+    Open-Source Code</a>
+     by Krista Thompson
+    </small>
+</div>
+  );
+  }
