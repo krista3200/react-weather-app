47e435c16eb9131dfdbd449ec77d9af98d68e8c0
krista3200 <73085133+krista3200@users.noreply.github.com>
2021-03-07 11:43:28 -0600

    added rel=noreferrer to footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
 <div>
 <small>
-  <a href="https://github.com/krista3200/weather-project" target="_blank">
-    Open-Source Code</a>
+  <a href="https://github.com/krista3200/react-weather-app" target="_blank" rel="noreferrer">
+    Open-Source Code</a> {" "}
      by Krista Thompson
     </small>
 </div>
